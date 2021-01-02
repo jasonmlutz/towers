23e81449fe0edf496cbd092f3c2f9356579c9a28
Jason Lutz <jason.m.lutz@gmail.com>
2021-01-01 19:56:35 -0800

    Add first implementation of promptMove and isValidMove methods

--- towers.js
+++ towers.js
@@ -1,9 +1,44 @@
 class Game {
   constructor() {
-    this.towers = [[], [], []]
+    this.towers = [[4, 3, 2, 1], [], []]
   }
 
   run () {
 
   }
 }
+
+Game.prototype.promptMove = function () {
+  // print the stacks
+  this.towers.forEach(tower => {
+    let towerDisplay = '|' + tower.join('-');
+    console.log(towerDisplay);
+  })
+  // ask user for move selection
+  console.log('please select starting tower for move')
+
+  console.log('please select ending tower for move')
+};
+
+Game.prototype.isValidMove = function (startTowerIdx, endTowerIdx) {
+  // return false if startTower is empty
+  let startTower = this.towers[startTowerIdx];
+  let endTower = this.towers[endTowerIdx];
+  if (startTower.length === 0) {
+    return false
+  }
+  // return true if endTower is empty
+  if (endTower.length === 0) {
+    return true
+  }
+  // compare 'top' elements of startTower and endTower
+  let startTowerTop = startTower[startTower.length - 1];
+  let endTowerTop = endTower[endTower.length - 1];
+  if (startTowerTop < endTowerTop) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+game = new Game;
