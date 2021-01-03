24697594a04bdcf621d56fe647f89dd6db86bc39
Jason Lutz <jason.m.lutz@gmail.com>
2021-01-02 20:43:26 -0800

    Build playScript with run call and completion

--- playScript.js
+++ playScript.js
@@ -1,1 +1,20 @@
 const Game = require("./towers");
+const readline = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let g = new Game();
+g.run(reader, completion);
+
+function completion() {
+  reader.question('play again? y or no: ', restartGame => {
+    if (restartGame === "y") {
+      g = new Game();
+      g.run(reader, completion);
+    } else {
+      reader.close();
+    }
+  });
+};
